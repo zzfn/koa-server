a39c4347008a5d8ec3d7773a8068c10675e1987e
chenc <git@zzfzzf.com>
2021-07-09 23:50:57 +0800

    feat: 日志

--- routes/monitor.js
+++ routes/monitor.js
@@ -1,18 +1,21 @@
 const Router = require('koa-router')
 const MongoClient = require('mongodb').MongoClient;
 const url = require('../mongodb/config')
-const client = new MongoClient(url);
+
 
 const router = new Router({prefix: '/monitor'})
 
 router.post('/trace', async ctx => {
     const req = ctx.request.body
+    const client = new MongoClient(url);
     try {
         await client.connect();
         const re =await client.db("zzf").collection('logs').insertOne({...req,time:new Date()});
         ctx.body=re.result
     } finally {
-        await client.close();
+        if(client){
+            await client.close();
+        }
     }
 })
 
