270bd1ada3a29857841b08217396cdcd129418ec
chenc <chenchen3@fosun.com>
2021-08-18 23:56:57 +0800

    feat: 查询

--- routes/monitor.js
+++ routes/monitor.js
@@ -10,7 +10,7 @@ router.post('/trace', async ctx => {
     const client = new MongoClient(url);
     try {
         await client.connect();
-        const re =await client.db("zzf").collection('logs').insertOne({...req,time:new Date(),ip:ctx.request.ip});
+        const re =await client.db("zzf").collection('logs').insertOne({...req,time:new Date().toLocaleString(),ip:ctx.request.ip});
         ctx.body=re.result
     } finally {
         if(client){
@@ -18,5 +18,18 @@ router.post('/trace', async ctx => {
         }
     }
 })
-
+router.get('/trace', async ctx => {
+    const req = ctx.request.body
+    const client = new MongoClient(url);
+    try {
+        await client.connect();
+        const re =await client.db("zzf").collection('logs').find({}).toArray();
+        console.log(re)
+        ctx.body=re
+    } finally {
+        if(client){
+            await client.close();
+        }
+    }
+})
 module.exports = router
